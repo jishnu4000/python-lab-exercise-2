abe915289c4572a0e345a96e6cd692b67b3a596e
Jishnu <jishnu.jayaraj@mca.christuniversity.in>
2024-07-28 12:47:10 +0530

    added comments

--- module_ListFunction.py
+++ module_ListFunction.py
@@ -1,15 +1,22 @@
+# Question 1
+
+# a) i
 def maximum_value(arr):
   return max(arr)
 
+# a) ii
 def minimum_value(arr):
   return min(arr)
 
+# a) iii
 def list_sum(arr):
   return sum(arr)
 
+# a) iv
 def list_average(arr):
   return sum(arr)/len(arr)
 
+# a) v
 def list_median(arr):
   arr.sort()
   if len(arr) % 2 != 0:
